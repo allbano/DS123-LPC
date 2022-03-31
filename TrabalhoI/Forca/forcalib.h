648be3eabd0d09951066cd88b011407a0263f3ce
Albano Maywitz <allbano@gmail.com>
2022-03-31 19:56:55 -0300

    Add header forcalib and reestructured folders game

---
+++ TrabalhoI/Forca/forcalib.h
@@ -0,0 +1,98 @@
+void forca (int tentativa){
+if(tentativa==0){
+printf("-----------------");
+printf("\n| |");
+printf("\n|");
+printf("\n|");
+printf("\n|");
+printf("\n|");
+printf("\n|");
+printf("\n__");
+}
+else if(tentativa==1){
+printf("-----------------");
+printf("\n| |");
+printf("\n| 0");
+printf("\n|");
+printf("\n|");
+printf("\n|");
+printf("\n|");
+printf("\n__");
+}
+else if(tentativa==2){
+printf("-----------------");
+printf("\n| |");
+printf("\n| 0");
+printf("\n| |");
+printf("\n| |");
+printf("\n|");
+printf("\n|");
+printf("\n__");
+}
+else if(tentativa==3){
+printf("-----------------");
+printf("\n| |");
+printf("\n| 0");
+printf("\n| -|");
+printf("\n| |");
+printf("\n|");
+printf("\n|");
+printf("\n__");
+}
+else if(tentativa==4){
+printf("-----------------");
+printf("\n| |");
+printf("\n| 0");
+printf("\n| -|-");
+printf("\n| |");
+printf("\n|");
+printf("\n|");
+printf("\n__");
+}
+else if(tentativa==5){
+printf("-----------------");
+printf("\n| |");
+printf("\n| 0");
+printf("\n| -|-");
+printf("\n| |");
+printf("\n| /");
+printf("\n|");
+printf("\n__");
+}
+else if(tentativa==6){
+printf("-----------------");
+printf("\n| |");
+printf("\n| 0");
+printf("\n| -|-");
+printf("\n| |");
+printf("\n| / \\");
+printf("\n|");
+printf("\n__ Você perdeu! :(");
+}
+}
+
+
+/*
+void forca (int tentativa){
+
+    switch (variável) {
+        case 0 :
+            printf("-----------------");
+            printf("\n| |");
+            printf("\n|");
+            printf("\n|");
+            printf("\n|");
+            printf("\n|");
+            printf("\n|");
+            printf("\n__");
+        break;
+
+        case  1 :
+     Instruções;
+   break;
+
+   default
+     Instruções;
+}
+}
+*/
---
+++ TrabalhoI/Forca/main.c
@@ -0,0 +1,79 @@
+//Bibliotecas padrão da Linguagem C
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+//Bibliotecas específicas
+#include <time.h>
+#include <string.h>
+//Biblioteca do Jogo
+#include "forcalib.h"
+
+//Variáveis Globais
+char banco[10][10]= {"cachorro", "morango", "bicicleta", "curitiba", "computador", "cereal", "televisao", "faculdade", "trabalho", "girafa"};
+
+
+
+
+
+int main (void){
+
+int i;
+int num=0;
+char palavra[10];
+char tela[10];
+
+
+srand(time(NULL)); //biblioteca time.h
+num = rand() % 10; //escolhe palavra aleatoria
+for(i=0; i<10;i++){
+palavra[i]=banco[num][i];
+}
+strcpy(tela, palavra); //grava o conteudo da char palavra em char tela
+
+
+
+
+for (int i=0; i<strlen(tela);i++){ // transforma a palavra secreta da tela em _
+tela[i]=('_'); //ESPACO
+}
+// printf("A palavra é %s \n", tela);
+// scanf("%c", &palavra);
+
+
+
+
+int erros = 0;
+while(1){
+forca(erros);
+printf("\n Adivinhe a palavra: ");
+for(int i=0;i<strlen(tela);i++){
+printf("%c ",tela[i]);
+}
+
+
+printf("\nLetra: ");
+char letra;
+scanf(" %c", &letra);
+
+int letra_errada=1;
+for(int i=0;i<strlen(palavra); i++){
+if(letra==palavra[i]){
+tela[i]=letra;
+letra_errada=0;
+}
+}
+if(letra_errada==1){
+erros++;
+}
+
+if(strcmp(tela,palavra)==0){
+printf("Você venceu! :)");
+}
+
+if(erros==6){
+forca(erros);
+break;
+}
+}
+return 0;
+}
