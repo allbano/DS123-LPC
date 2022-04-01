5ab30722dd569c84b5083c460bd7d81f50e7e6c1
Albano Maywitz <allbano@gmail.com>
2022-04-01 00:10:43 -0300

    Jogo da Velha project Started

---
+++ TrabalhoI/JogodaVelha/velha.c
@@ -0,0 +1,28 @@
+/*  Bibliotecas Base */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*  Bibliotecas Específicas */
+#include <locale.h>
+#include <string.h>
+#include <time.h>
+
+/*  Bibliotecas do Jogo */
+#include "velhalib.h"
+
+
+/*  Prototype de funções */
+int menu();
+int jogada_usuario(int lin,int col,char jog);
+void jogada_computador(char jog,int nivel);
+void escolha_simb(char *jog1, char *jog2);
+void inicializa_velha();
+int verifica_ganhador(char jog);
+
+/*  Variáveis Globais */
+char jdv[3][3];
+
+int main(){
+
+return 0;
+}
---
+++ TrabalhoI/JogodaVelha/velhalib.h
@@ -0,0 +1,9 @@
+/*  Prototype Funtions */
+int menu();
+int jogada_usuario(int lin,int col,char jog);
+void jogada_computador(char jog,int nivel);
+void escolha_simb(char *jog1, char *jog2);
+void inicializa_velha();
+int verifica_ganhador(char jog);
+
+/*  Funtions */
