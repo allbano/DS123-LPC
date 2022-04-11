647f2331991c0eba5afc3ea6a1ab6c940f1e106a
Albano Maywitz <allbano@gmail.com>
2022-04-11 02:22:13 -0300

    Jogo da Velha project finished the functions: menu(); escolha_simb(); inicializa_velha();

--- TrabalhoI/JogodaVelha/velha.c
+++ TrabalhoI/JogodaVelha/velha.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 /*  Bibliotecas Específicas */
+#include <ctype.h>
 #include <locale.h>
 #include <string.h>
 #include <time.h>
@@ -11,18 +12,70 @@
 #include "velhalib.h"
 
 
-/*  Prototype de funções */
-int menu();
-int jogada_usuario(int lin,int col,char jog);
-void jogada_computador(char jog,int nivel);
-void escolha_simb(char *jog1, char *jog2);
-void inicializa_velha();
-int verifica_ganhador(char jog);
+/*  Constantes */
 
 /*  Variáveis Globais */
-char jdv[3][3];
+char jdv[3][3], *gg=&jdv[0][0],jg1=' ',jg2=' ';
+int jogc=0;
+
+
+
+/*  Prototype de funções */
+// Com retorno
+int menu();
+int jogada_usuario(int lin,int col,char jog);
+int verifica_ganhador(char jog);
+// Sem retorno
+void jogada_computador(char jog,int nivel);
+void escolha_simb(char *jog1, char *jog2); //Feita
+void inicializa_velha(char *jogv); //Feita
+
+
 
 int main(){
 
+jogc=menu();
+inicializa_velha(gg);
+escolha_simb(&jg1,&jg2);
+
+printf("\nJogando contra: %d", jogc);
+printf("\nJogador 1: %c", jg1);
+printf("\nJogador 2: %c", jg2);
+
+
 return 0;
 }
+
+
+
+/*
+int l;
+for (l=0;l<9;l++){
+            printf("%c  \n", *(gg+l));
+            }
+ printf("\nVerificação...\n");
+
+*(gg+3)='9';
+
+for (l=0;l<9;l++){
+            printf("%c  \n", *(gg+l));
+            }
+
+escolha_simb(&jg1,&jg2);
+printf("\nJogador 1: %c", jg1);
+printf("\nJogador 2: %c", jg2);
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
--- TrabalhoI/JogodaVelha/velhalib.h
+++ TrabalhoI/JogodaVelha/velhalib.h
@@ -1,9 +1,99 @@
 /*  Prototype Funtions */
+// Com retorno
 int menu();
 int jogada_usuario(int lin,int col,char jog);
-void jogada_computador(char jog,int nivel);
-void escolha_simb(char *jog1, char *jog2);
-void inicializa_velha();
 int verifica_ganhador(char jog);
 
+// Sem retorno
+void inicializa_velha(char *jogv);
+void jogada_computador(char jog,int nivel);
+void escolha_simb(char *jog1, char *jog2);
+
+
 /*  Funtions */
+int menu(){
+    int escolha,erro=1;
+    do {
+        printf("\n====== MENU JOGO DA VELHA ======\n\n\n");
+        printf("(1) Se quiser jogar contra o computador.\n");
+        printf("(2) Se quiser jogar contra outro usuário.\n");
+        printf("(0) Se quiser sair.\n");
+        printf("Escolha uma das opções do Menu: ");
+        scanf("%d",&escolha);
+
+        //printf("O valor da escolha é: %d", escolha);
+        switch(escolha){
+            case 0:{
+                exit(0);
+                break;
+                }
+            case 1:  {
+                erro=0;
+                break; }
+            case 2:  {
+                erro=0;
+                break; }
+            default:
+                printf("\nDigite uma opção válida!!!");
+        }
+    }while(erro==1);
+return escolha;
+}
+
+void inicializa_velha(char *jogv) {
+        int l;
+        for (l=0;l<9;l++){
+                *(jogv+l)=' ';
+
+        }
+}
+
+/* void escolha_simb(char *jog1, char *jog2) {
+        char escolha;
+        do{
+            printf("\nSelecione qual caracter/símbolo o jogador '1' usará - 'X' ou 'O': ");
+            scanf("%c%*c", &escolha);
+            escolha = toupper(escolha);
+            if ((escolha == 'X') || (escolha == 'O')) {
+                break;
+                } else {
+                    printf("Opção inválida! Favor digitar corretamente a opção 'X' ou 'O' (maiúsculo ou minúsculo!)\n");
+                }
+        }while((escolha != 'X') || (escolha != 'O'));
+        if(escolha=='X') {
+            *jog1=escolha;
+            *jog2='O';
+            } else {
+                *jog1=escolha;
+                *jog2='X';
+                }
+} */
+
+
+void escolha_simb(char *jog1, char *jog2) {
+        char escolha;
+        int valor=0;
+        do{
+            printf("\nSelecione qual caracter/símbolo o jogador '1' usará - 'X' ou 'O': ");
+            scanf("%*c%c", &escolha);
+            escolha = toupper(escolha);
+
+            //printf("\nO valor da escolha é: %c", escolha);
+            switch(escolha){
+            case 'X':{
+                *jog1=escolha;
+                *jog2='O';
+                valor=1;
+                break;
+                }
+            case 'O':{
+                *jog1=escolha;
+                *jog2='X';
+                valor=1;
+                break;
+                }
+            default:
+                printf("Opção inválida! Favor digitar corretamente a opção 'X' ou 'O' (maiúsculo ou minúsculo!)\n");
+            }
+          }while(valor==0);
+}
